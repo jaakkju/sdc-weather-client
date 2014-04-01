f058302b32917f49b9f4ccec69d53621713216ce
jarkkom <jarkkom@dhcp-eduroam-hy-55-45.mobile.helsinki.fi>
2014-04-01 18:22:32 +0300

    use Measurement data model instead of sampledata

--- src/uni/helsinki/sdc_weather/Weather.java
+++ src/uni/helsinki/sdc_weather/Weather.java
@@ -1,6 +1,9 @@
 package uni.helsinki.sdc_weather;
 
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
@@ -12,15 +15,23 @@ import org.apache.http.impl.client.DefaultHttpClient;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import uni.helsinki.sdc_weather.model.Measurement;
+import uni.helsinki.sdc_weather.model.MeasurementDataService;
 import android.app.Activity;
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationManager;
+import android.location.LocationProvider;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.text.method.DateTimeKeyListener;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.sensorcon.sensordrone.Humidity_V1;
 import com.sensorcon.sensordrone.android.Drone;
 
 public class Weather extends Activity {
@@ -49,24 +60,36 @@ public class Weather extends Activity {
 		    	new DoGet(){
 
 					@Override
-					protected void onPostExecute(String result) {
+					protected void onPostExecute(List<Measurement> result) {
 						TextView resultView = (TextView) findViewById(R.id.textView_result);
-						resultView.setText(result);
+						resultView.setText(result.toString());
 					}
 		    	}.execute();
 		    }
 		});
 		
+		final Context thisContext = this;
+		
 		buttonPost = (Button) findViewById(R.id.ButtonPost);
 		buttonPost.setOnClickListener(new View.OnClickListener() {
 		    public void onClick(View v) {
-				try {
-					// Using HTTP Post with sample data
-					JSONObject jsonObj = new JSONObject(SAMPLE_JSON);
-					new DoPost().execute(jsonObj);
-				} catch (JSONException e) {
-					Log.i(TAG, "JSON Parsing failed", e);
+		    	
+		    	LocationManager lm = (LocationManager)thisContext.getSystemService(Context.LOCATION_SERVICE);
+		    	
+		    	Location loc = lm.getLastKnownLocation(LOCATION_SERVICE);
+		    	
+				// Using HTTP Post with sample data					
+				Measurement m = new Measurement();
+				m.setTimestamp(new Date());
+				if (loc != null) {
+					m.setLatitude(loc.getLatitude());
+					m.setLongitude(loc.getLongitude());
 				}
+				m.setPressureAtmospheres(0.99);
+				m.setTemperatureCelsius(37.0);
+				m.setHumidity(42.42);
+					
+				new DoPost().execute(m);
 		    }
 		});
 		
@@ -96,10 +119,10 @@ public class Weather extends Activity {
 		return true;
 	}
 	
-	private class DoMeasure extends AsyncTask<Void, Void, Boolean> {
+	private class DoMeasure extends AsyncTask<Void, Void, Measurement> {
 
 		@Override
-		protected Boolean doInBackground(Void... arg0) {
+		protected Measurement doInBackground(Void... arg0) {
 			Log.i(TAG, "Measuring Temperature: " + String.valueOf(drone.measureTemperature()));
 			Log.i(TAG, "Measuring Humididy: " + String.valueOf(drone.measureHumidity()));
 			Log.i(TAG, "Measuring Pressure: " + String.valueOf(drone.measurePressure()));
@@ -108,23 +131,32 @@ public class Weather extends Activity {
 			Log.i(TAG, "Humidity value: " + String.valueOf(drone.humidity_Percent));
 			Log.i(TAG, "Pressure value: " + String.valueOf(drone.pressure_Atmospheres));
 			
-			return true;
+			Measurement m = new Measurement();
+			m.setHumidity(drone.humidity_Percent);
+			m.setPressureAtmospheres(drone.pressure_Atmospheres);
+			m.setTemperatureCelsius(drone.temperature_Celsius);
+			
+			return m;
 		}
 	}
 	
-	private class DoPost extends AsyncTask<JSONObject, Void, Boolean> {
+	private class DoPost extends AsyncTask<Measurement, Void, Boolean> {
 		
 		@Override
-		protected Boolean doInBackground(JSONObject... params) {
-		Boolean result = true;
+		protected Boolean doInBackground(Measurement... params) {
+			Boolean result = true;
 
+			MeasurementDataService mds = new MeasurementDataService();
+			
+			DefaultHttpClient httpclient = new DefaultHttpClient();
+			
 			// Post all JSON objects
 			for (int i = 0; i < params.length; i++) {
 			    try {
-					DefaultHttpClient httpclient = new DefaultHttpClient();
-					HttpPost httpPostRequest = new HttpPost(URL);
 
-					StringEntity se = new StringEntity(params[i].toString());
+			    	HttpPost httpPostRequest = new HttpPost(URL);
+
+					StringEntity se = new StringEntity(mds.toJson(params[i]));
 
 					// Set HTTP parameters
 					httpPostRequest.setEntity(se);
@@ -149,10 +181,10 @@ public class Weather extends Activity {
 		}
 	}
 	
-	private class DoGet extends AsyncTask<Void, Void, String> {
+	private class DoGet extends AsyncTask<Void, Void, List<Measurement>> {
 
 		@Override
-		protected String doInBackground(Void... params) {
+		protected List<Measurement> doInBackground(Void... params) {
 			String result = "";
 
 			try {
@@ -167,6 +199,7 @@ public class Weather extends Activity {
 				if (entity != null) {
 					InputStream inStream = entity.getContent();
 					result = convertStreamToString(inStream);
+					
 					inStream.close();
 					
 				} else {
@@ -176,7 +209,11 @@ public class Weather extends Activity {
 			} catch (Exception e) {
 				Log.e(TAG, "Connection failed: ", e);
 			}
-			return result;
+			try {
+				return new MeasurementDataService().fromResultJson(result);
+			} catch (Exception e) {
+				return new ArrayList<Measurement>();
+			}
 		}
 	}
 
